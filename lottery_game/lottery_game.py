3a3b589fa585e58427191a843db7edba3fe31ad5
Leonardo Serra <112865257+leonardoserra@users.noreply.github.com>
2024-07-09 09:33:51 +0200

    Update lottery_game.py

--- lottery_game/lottery_game.py
+++ lottery_game/lottery_game.py
@@ -1,7 +1,11 @@
+# lottery game, there are 15 winning numbers, each player will extract a number 
+# till the first that found 3 numbers win!
+# pass a list of LotteryPlayer in the start_game() function
+
+#start the program and see who will win
+
 from lottery import Lottery
 from lottery_player import LotteryPlayer
-
-Lottery.extract_winning_numbers() #creata set n vincenti
 
 players:list[LotteryPlayer] = [
   LotteryPlayer('Leonardo'),
